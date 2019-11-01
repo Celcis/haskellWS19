fca80210497c707d34e4921955444f0e6a8c3c45
Mehmet Ali Baykara <31475871+Celcis@users.noreply.github.com>
2019-11-01 23:43:23 +0100

    Update Uebung02.hs

--- Source/Blatt02/Uebung02.hs
+++ Source/Blatt02/Uebung02.hs
@@ -1,1 +1,50 @@
 
+module Uebung_2 where
+
+import Data.List
+
+-- | Folgende Funktion prüft, ob erste Parameter ein prefix von zweite Parameter ist. 
+startStrOf :: String -> String -> Bool
+startStrOf "" y = True
+startStrOf x "" = False
+startStrOf (x:xs) (y:ys) = x == y && startStrOf xs ys  
+
+-- | Folgende Funtion prüft, ob erste Parameter in der zweite Parameter vorkommt. 
+subStrOf :: String -> String -> Bool
+subStrOf "" y = True
+subStrOf x "" = False
+subStrOf x y = isInfixOf x y
+
+-- | Prüft, ob eingegebene char eine konsonant ist
+isNotVowel :: Char -> Bool
+isNotVowel c = notElem c "aeiou"
+
+-- | Jede Konsonant wird verdoppelt und zwischen ein 'o' eingefügt.
+robber :: String -> String
+robber [] = []
+robber (x:xs)
+    | length (x:xs) < 1 = ""
+    | isNotVowel x = x : 'o' : x : robber xs 
+    | True = x : robber xs 
+
+
+
+-- | Prüft, ob eingegebene char eine consonant ist
+consonant :: Char -> Bool
+consonant c = notElem c "aeiou"
+
+-- | Prüft, ob char eine vokal ist
+isVowel:: Char -> Bool
+isVowel c
+    |elem c "aeiou" = True
+    | True = False
+    
+
+ 
+-- | Diese Frunktion ist eine dekodierung von robber ist. 
+unrobber :: String -> String
+unrobber [] = []
+unrobber str  
+    | (head str) == ' ' = " " ++ unrobber (drop 1 str)
+    | consonant (head str) = [head str] ++ unrobber (drop 3 str)
+    | True = [head str] ++ unrobber (drop 1 str)

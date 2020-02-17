2ed6c5dc52d986ca6d3df3d8e142715da30363a1
Mehmet Ali Baykara <mehmetalibaykara@gmail.com>
2020-02-18 00:29:54 +0100

    doku

--- exam/ElimEvensDoubleOdds.hs
+++ exam/ElimEvensDoubleOdds.hs
@@ -1,7 +1,12 @@
 module ElimEvensDoubleOdds where
 
 
+-- | using list comprehension
 elimEvensDoubleOdds :: [Int] -> [Int]
 elimEvensDoubleOdds ls = [x * 2 | x <-ls, x `mod` 2 /=0] 
 
+-- | eliminate even number and multiply each one using lambda expression 
 elim2 ls = map (*2)(filter (\x -> odd x ) ls)
+
+
+
